11a715f6fcaf340d15817ba397549ec35c800fbe
mohammedabyan <mohammedabyan22@gmail.com>
2023-05-22 03:13:53 +0800

    Update on input for simple system

--- finly-code/resources/views/simpleinput.blade.php
+++ finly-code/resources/views/simpleinput.blade.php
@@ -2,9 +2,62 @@
 
 
 @section('content')
-    @parent
-    <h1>TEST</h1>s
-    <h1>TEST</h1>
-    <h2>H2</h2>
+            <!-- general form elements -->
+            <div class="card card-primary">
+                <div class="card-header">
+                <h3 class="card-title">Quick Example</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form>
+                <div class="card-body">
+                    <div class="form-group">
+                    <label for="Sales">Sales / Gross Revenue</label><i class="fas fa-info-circle" @popper(TOOL TIP DESC)> </i>
+                    <input type="number" class="form-control" id="Sales" placeholder="Enter a number" min=0>
+                    </div>
+
+                    <div class="form-group">
+                    <label for="Sales">Assets</label><i class="fas fa-info-circle" @popper(TOOL TIP DESC)> </i>
+                    <input type="number" class="form-control" id="Assets" placeholder="Enter a number" min=0>
+                    </div>
+
+                    <div class="form-group">
+                    <label for="Sales">Inventory</label><i class="fas fa-info-circle" @popper(TOOL TIP DESC)> </i>
+                    <input type="number" class="form-control" id="Sales" placeholder="Enter a number" min=0>
+                    </div>
+                </div>
+                <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+       
+
+        
+            <!-- general form elements -->
+            <div class="card card-primary">
+                <div class="card-header">
+                <h3 class="card-title">Quick Example</h3>
+                </div>
+                <!-- /.card-header -->
+                <!-- form start -->
+                <form>
+                <div class="card-body">
+                    <div class="form-group">
+                    <label for="COGS">COGS</label><i class="fas fa-info-circle" @popper(TOOL TIP DESC)> </i>
+                    <input type="number" class="form-control" id="COGS" placeholder="Enter a number" min=0>
+                    </div>
+
+                    <div class="form-group">
+                    <label for="Liabilities">Liabilities</label><i class="fas fa-info-circle" @popper(TOOL TIP DESC)> </i>
+                    <input type="number" class="form-control" id="Liabilities" placeholder="Enter a number" min=0>
+                    </div>
+
+                    <div class="form-group">
+                    <label for="Operating Expenses">Inventory</label><i class="fas fa-info-circle" @popper(TOOL TIP DESC)> </i>
+                    <input type="number" class="form-control" id="operatingExpenses" placeholder="Enter a number" min=0>
+                    </div>
+                </div>
+                <!-- /.card-body -->         
+            </div>
+            <!-- /.card -->
 @stop
 
