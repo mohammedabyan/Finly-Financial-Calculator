91795b456f73ad1a01ba06a4cea5e0af1cae849c
mohammedabyan <mohammedabyan22@gmail.com>
2023-05-21 13:25:53 +0800

    Base files

---
+++ finly-code/resources/views/simpleinput.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+
+@section('content')
+    @parent
+    <h1>TEST</h1>s
+    <h1>TEST</h1>
+    <h2>H2</h2>
+@stop
+
---
+++ finly-code/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('d1');
+});
+
+Route::get('/d1', function () {
+    return view('d1');
+});
+
+Route::get('/d2', function () {
+    return view('d2');
+});
+
+Route::get('/d3', function () {
+    return view('d3');
+});
+
+Route::get('/master', function () {
+    return view('layouts/master');
+});
+
+Route::get('/simpleinput', function () {
+    return view('simpleinput');
+});
+
