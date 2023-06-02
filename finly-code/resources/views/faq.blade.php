5c8ffcf71c3c6701f5987710bce84c590a6c347c
hariszvki <aniqharis181@gmail.com>
2023-06-02 16:20:17 +0800

    other-nav

--- finly-code/resources/views/faq.blade.php
+++ finly-code/resources/views/faq.blade.php
@@ -30,13 +30,14 @@
                     <a class="d-block w-100" data-toggle="collapse" href="#collapseOne">
                         <div class="card-header">
                             <h4 class="card-title w-100">
-                                1. Lorem ipsum dolor sit amet
+                                1. What is Finly, a financial health checker for SMEs?
                             </h4>
                         </div>
                     </a>
                     <div id="collapseOne" class="collapse show" data-parent="#accordion">
                         <div class="card-body">
-                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                            The Finly Financial Health Checker for SMEs is a specialized tool that evaluates and analyzes the financial well-being of small and medium-sized enterprises.
+                            It provides valuable insights into the company's financial performance and stability by assessing various financial indicators.
                         </div>
                     </div>
                 </div>
@@ -44,13 +45,14 @@
                     <a class="d-block w-100" data-toggle="collapse" href="#collapseTwo">
                         <div class="card-header">
                             <h4 class="card-title w-100">
-                                2. Aenean massa
+                                2. How does Finly work?
                             </h4>
                         </div>
                     </a>
                     <div id="collapseTwo" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                            The Finly Financial Health Checker utilizes advanced algorithms and financial analysis techniques to assess the overall financial health of an SME.
+                            By analyzing factors such as cash flow, profitability, liquidity, debt levels, and other relevant financial metrics, it generates comprehensive reports and recommendations.
                         </div>
                     </div>
                 </div>
@@ -58,13 +60,15 @@
                     <a class="d-block w-100" data-toggle="collapse" href="#collapseThree">
                         <div class="card-header">
                             <h4 class="card-title w-100">
-                                3. Donec quam felis
+                                3. What information is required to use the Finly Financial Health Checker?
                             </h4>
                         </div>
                     </a>
                     <div id="collapseThree" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                            To use the Finly Financial Health Checker, you need to input your SME's financial statements, including balance sheets' metric, income statements, and cash flow statements.
+                            It is to be noted that the Finly has 2 interfaces for beginners and advanced users. Depending on your level of expertise, you can choose the interface that best suits your needs.
+                            Additionally, providing information about your industry and market conditions can enhance the accuracy of the assessment.
                         </div>
                     </div>
                 </div>
@@ -72,13 +76,14 @@
                     <a class="d-block w-100" data-toggle="collapse" href="#collapseFour">
                         <div class="card-header">
                             <h4 class="card-title w-100">
-                                4. Donec pede justo
+                                4. What are the benefits of using the Finly Financial Health Checker for my SME?
                             </h4>
                         </div>
                     </a>
                     <div id="collapseFour" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+                            The Finly Financial Health Checker offers numerous benefits for your SME. It helps you gain insights into your financial performance, identify risks,
+                            make informed decisions, and devise strategies to enhance financial stability and growth potential.
                         </div>
                     </div>
                 </div>
@@ -86,13 +91,14 @@
                     <a class="d-block w-100" data-toggle="collapse" href="#collapseFive">
                         <div class="card-header">
                             <h4 class="card-title w-100">
-                                5. In enim justo
+                                5. Can the Finly Financial Health Checker assist with financial planning and forecasting?
                             </h4>
                         </div>
                     </a>
                     <div id="collapseFive" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+                            Absolutely! The Finly Financial Health Checker aids in financial planning and forecasting by analyzing historical financial data and identifying trends.
+                            It provides a solid foundation for making projections, setting goals, and creating effective financial strategies for the future.
                         </div>
                     </div>
                 </div>
@@ -100,13 +106,14 @@
                     <a class="d-block w-100" data-toggle="collapse" href="#collapseSix">
                         <div class="card-header">
                             <h4 class="card-title w-100">
-                                6. Integer tincidunt
+                                6.How frequently should I use the Finly Financial Health Checker for my SME?
                             </h4>
                         </div>
                     </a>
                     <div id="collapseSix" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+                            It is recommended to use the Finly Financial Health Checker periodically, such as quarterly or annually, to track changes in your SME's financial health over time.
+                            Regular assessments enable you to monitor progress, detect emerging issues, and take proactive measures to maintain a healthy financial position.
                         </div>
                     </div>
                 </div>
@@ -114,13 +121,14 @@
                     <a class="d-block w-100" data-toggle="collapse" href="#collapseSeven">
                         <div class="card-header">
                             <h4 class="card-title w-100">
-                                7. Aenean leo ligula
+                                7. Is my data secure and confidential when using the Finly Financial Health Checker?
                             </h4>
                         </div>
                     </a>
                     <div id="collapseSeven" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+                            Most definitely! At Finly, we prioritize data security and confidentiality. Our Finly Financial Health Checker employs robust encryption measures and adheres to strict data protection protocols.
+                            Your financial information remains secure and confidential, and we do not share it with any third parties without your explicit consent.
                         </div>
                     </div>
                 </div>
@@ -128,13 +136,14 @@
                     <a class="d-block w-100" data-toggle="collapse" href="#collapseEight">
                         <div class="card-header">
                             <h4 class="card-title w-100">
-                                8. Aliquam lorem ante
+                                8. Can I customize the Finly Financial Health Checker to suit my SME's unique needs?
                             </h4>
                         </div>
                     </a>
                     <div id="collapseEight" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.
+                            While the Finly Financial Health Checker provides a standardized analysis based on industry standards and best practices, some customization options may be available.
+                            Feel free to reach out to our support team to discuss your specific requirements and explore potential customization options.
                         </div>
                     </div>
                 </div>
@@ -142,13 +151,14 @@
                     <a class="d-block w-100" data-toggle="collapse" href="#collapseNine">
                         <div class="card-header">
                             <h4 class="card-title w-100">
-                                9.  Quisque rutrum
+                                9. How accurate is the assessment provided by the Finly Financial Health Checker?
                             </h4>
                         </div>
                     </a>
                     <div id="collapseNine" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+                            The accuracy of the assessment generated by the Finly Financial Health Checker depends on the quality and accuracy of the financial data you provide.
+                            It is crucial to ensure that the data entered is up-to-date and reflects the true financial position of your company for the assessment to be accurate.
                         </div>
                     </div>
                 </div>
@@ -157,7 +167,7 @@
         <div class="row">
             <div class="col-12 mt-3 text-center">
                 <p class="lead">
-                    <a href="contact-us.html">Contact us</a>,
+                    <a href="contact">Contact us</a>,
                     if you found not the right anwser or you have a other question?<br />
                 </p>
             </div>
--- finly-code/resources/views/layouts/ogmaster.blade.php
+++ finly-code/resources/views/layouts/ogmaster.blade.php
@@ -71,6 +71,30 @@
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="home" class="nav-link">Dashboard</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="simpleinput" class="nav-link">Simple</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="advancedinput" class="nav-link">Advanced</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="planner" class="nav-link">Planner</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="contact" class="nav-link">Contact</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="faq" class="nav-link">FAQ</a>
+      </li>
+    </ul>
 
     <ul class="navbar-nav ml-auto">
         <!-- Authentication Links -->
